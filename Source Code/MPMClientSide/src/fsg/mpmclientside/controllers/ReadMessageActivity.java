bfbf5258f0b1bbf97077a6fb30f8fa50085d04e8
Lương Anh Sơn <sonla01889@fpt.edu.vn>
2013-07-13 10:55:07 +0700

    up code 3 file xml
    send_message.xml
    read_message.xml
    user_profile.xml

--- Source Code/MPMClientSide/src/fsg/mpmclientside/controllers/ReadMessageActivity.java
+++ Source Code/MPMClientSide/src/fsg/mpmclientside/controllers/ReadMessageActivity.java
@@ -1,21 +1,24 @@
-package fsg.mpmclientside;
+package fsg.mpmclientside.controllers;
 
+import fsg.mpmclientside.R;
+import fsg.mpmclientside.R.layout;
+import fsg.mpmclientside.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class PopupMenu extends Activity {
+public class ReadMessageActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_popup_menu);
+		setContentView(R.layout.activity_read_message);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.popup_menu, menu);
+		getMenuInflater().inflate(R.menu.read_message, menu);
 		return true;
 	}
 
