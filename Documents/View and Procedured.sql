0e9b40d474f5daf0f6547a509f27a678f0665cfb
KhanhVD <khanhvuduy@hotmail.com>
2013-07-16 06:59:04 +0700

    update 7 h 16/07

--- Documents/View and Procedured.sql
+++ Documents/View and Procedured.sql
@@ -1,2 +1,9 @@
 -----------------SQL Views-----------------
+CREATE VIEW view_name AS
+SELECT *
+FROM Users
+WHERE UserID = 1
+
 -----------------SQL Procedures-----------------
+
+CREATE procedure insertUser AS
